2dc1a9fa1fb4d4cf375cdf838f53c4fa58d8a50d
Suetosha <honey.pancake2017@yandex.ru>
2025-03-25 11:36:53 +0100

    Добавлено поле photo в таблицу apartments

--- database/create_tables.py
+++ database/create_tables.py
@@ -5,20 +5,29 @@ def init_db():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
 
-    # Создание таблицы пользователей
-    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        user_id INTEGER UNIQUE)''')
+
+    # Создание таблицы пользователей с добавлением поля username
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            username TEXT,
+            UNIQUE(user_id, username)
+        )
+    ''')
 
 
     # Создание таблицы квартир
     cursor.execute('''CREATE TABLE IF NOT EXISTS apartments (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_id INTEGER NOT NULL,
                         title TEXT,
-                        price INTEGER,
+                        price TEXT,
                         city TEXT,
                         meters INTEGER,
-                        description TEXT)''')
+                        description TEXT,
+                        photo TEXT
+                    )''')
 
 
     # Создание таблицы активности пользователей
@@ -30,6 +39,7 @@ def init_db():
                         liked INTEGER DEFAULT 0,
                         FOREIGN KEY (user_id) REFERENCES users (user_id),
                         FOREIGN KEY (apartment_id) REFERENCES apartments (id))''')
+
 
     # Создание уникального индекса на сочетание user_id и apartment_id для предотвращения дублирования записей
     cursor.execute('''
