444681713848ba6dee38cdb47eebeca67c95cedb
Suetosha <honey.pancake2017@yandex.ru>
2025-03-25 11:36:05 +0100

    Добавлены обработчики коллбэков

--- utils/callback_factories.py
+++ utils/callback_factories.py
@@ -6,8 +6,23 @@ class DeleteFavoriteCallbackFactory(CallbackData, prefix='del'):
     apartment_id: int
     message_id: int
 
+class DeletePublishedCallbackFactory(CallbackData, prefix='del'):
+    apartment_id: int
+    message_id: int
+
 
 class GetApartmentCallbackFactory(CallbackData, prefix='get'):
+    user_id: int
+    apartment_id: int
+    message_id: int
+
+
+class GetPublishedApartmentCallbackFactory(CallbackData, prefix='get'):
+    apartment_id: int
+    message_id: int
+
+
+class EditPublishedApartmentCallbackFactory(CallbackData, prefix='edit'):
     user_id: int
     apartment_id: int
     message_id: int
