5575a7a87e7c625a9168eb81d6c7880ac8f0dc9e
Suetosha <honey.pancake2017@yandex.ru>
2025-03-25 11:26:27 +0100

    Добавлен parse_mode

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@ import asyncio
 import os
 
 from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
 from dotenv import load_dotenv
 
-from handlers import tenant_handlers, main_handlers, landlord_handlers
+from database.apartments import load_apartments_from_csv
+from handlers import tenant_handlers, main_handlers, landlord_handlers, other_handlers
 
 from database.create_tables import init_db
 from keyboards.main_menu import set_main_menu
@@ -16,17 +18,18 @@ async def main() -> None:
     try:
         token = os.getenv('TOKEN')
 
-        bot = Bot(token=token)
+        bot = Bot(token=token, default=DefaultBotProperties(parse_mode='HTML'))
         dp = Dispatcher()
 
         await set_main_menu(bot)
 
         init_db()
-        # load_apartments_from_csv()
+        load_apartments_from_csv()
 
         dp.include_router(main_handlers.router)
         dp.include_router(landlord_handlers.router)
         dp.include_router(tenant_handlers.router)
+        dp.include_router(other_handlers.router)
 
 
         await dp.start_polling(bot)
