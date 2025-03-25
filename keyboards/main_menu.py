1d379ee00786c9e12278406196d3b89662f24190
Suetosha <honey.pancake2017@yandex.ru>
2025-03-25 11:25:42 +0100

    Добавлены новые главные команды для бота

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -2,12 +2,13 @@ from aiogram import Bot
 from aiogram.types import BotCommand
 
 
-
 # Лексикон команд
 COMMAND_LEXICON = {
     '/filter': 'Ваш фильтр для поиска квартир',
     '/favorites': 'Посмотреть избранные квартиры',
-    '/view_apartments': 'Поиск квартир по фильтру'
+    '/view_apartments': 'Поиск квартир по фильтру',
+    '/publish_apartment': 'Опубликовать квартиру',
+    '/view_published_apartments': 'Посмотреть опубликованные квартиры'
 }
 
 
@@ -17,5 +18,10 @@ async def set_main_menu(bot: Bot):
         BotCommand(command='/filter', description=COMMAND_LEXICON['/filter']),
         BotCommand(command='/favorites', description=COMMAND_LEXICON['/favorites']),
         BotCommand(command="/view_apartments", description=COMMAND_LEXICON['/view_apartments']),
+        BotCommand(command="/publish_apartment", description=COMMAND_LEXICON['/publish_apartment']),
+        BotCommand(command="/view_published_apartments", description=COMMAND_LEXICON['/view_published_apartments']),
+
     ]
     await bot.set_my_commands(main_menu_commands)
+
+
