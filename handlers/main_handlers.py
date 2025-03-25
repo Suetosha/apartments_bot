cc269b1a7a2e1396690b77e018e2da5cbf4b9f59
Suetosha <honey.pancake2017@yandex.ru>
2025-03-25 11:27:42 +0100

    Добавлены хэндлеры к основным командам

--- handlers/main_handlers.py
+++ handlers/main_handlers.py
@@ -1,59 +1,59 @@
-from aiogram import Router, types, F
+from aiogram import Router, types
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message
 
 from dotenv import load_dotenv
-
+from lexicon.lexicon import LEXICON
 from database.activity import get_liked_apartments
+from database.apartments import get_apartments_by_landlord
 from database.filter import get_user_filters
 from database.users import save_user
-from keyboards.favorites_kb import favorites_kb
+from keyboards.tenant_kb import favorites_kb
+from keyboards.landlord_kb import landlord_apartments_kb
+from utils.fsm import FSMFillForm
 
 load_dotenv()
 router = Router()
 
-
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 
 @router.message(CommandStart())
 async def process_start_command(message: Message, state: FSMContext):
+    await state.clear()
     user_id = message.from_user.id
-    save_user(user_id)
+    username = message.from_user.username
+
+    save_user(user_id, username)
 
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Найти квартиру", callback_data="start_filter")],
         [InlineKeyboardButton(text="Сдать квартиру", callback_data="list_an_apartment")]
     ])
 
-    sent_message = await message.answer(
-        'Здравствуйте!\n'
-        'Вас приветствует бот для поиска и сдачи квартир.\n'
-        'Что вас интересует?',
-        reply_markup=keyboard
-    )
+    sent_message = await message.answer(LEXICON['start'], reply_markup=keyboard)
     await state.update_data(message_id=sent_message.message_id)
 
 
-
 @router.message(Command("view_apartments"))
-async def view_apartments_command(message: Message):
+async def view_apartments_command(message: Message, state: FSMContext):
+    await state.clear()
     # Отправляем сообщение с кнопкой, которая вызывает callback_query
-    button = types.InlineKeyboardButton(text="Начать просмотр", callback_data="view_apartments")
+    button = types.InlineKeyboardButton(text=LEXICON['start_view'], callback_data="view_apartments")
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=[[button]])
 
     # Отправляем сообщение с кнопкой
-    await message.answer("Нажмите кнопку для просмотра квартир:", reply_markup=keyboard)
-
-
+    await message.answer(LEXICON['press_for_view'], reply_markup=keyboard)
 
 
 @router.message(Command("filter"))
 async def filter_command(message: Message, state: FSMContext):
+    await state.clear()
     user_id = message.from_user.id
 
     # Отправляем сообщение с кнопкой, которая вызывает callback_query
-    button = types.InlineKeyboardButton(text="Редактировать фильтр", callback_data="start_filter")
+    button = types.InlineKeyboardButton(text=LEXICON['edit_filter'], callback_data="start_filter")
     keyboard = types.InlineKeyboardMarkup(inline_keyboard=[[button]])
 
     filter_data = get_user_filters(user_id)
@@ -66,18 +66,19 @@ async def filter_command(message: Message, state: FSMContext):
         # Отправляем сообщение с кнопкой
         sent_message = await message.answer(f"Ваш фильтр:\n{result_text}", reply_markup=keyboard)
     else:
-        sent_message = await message.answer('Фильтр ещё не создан.', reply_markup=keyboard)
+        sent_message = await message.answer(LEXICON['filter_not_created'], reply_markup=keyboard)
 
     await state.update_data(message_id=sent_message.message_id)
 
 
 @router.message(Command('favorites'))
-async def favorites_command(message: Message):
+async def favorites_command(message: Message, state: FSMContext):
+    await state.clear()
     user_id = message.from_user.id
     liked_apartments = get_liked_apartments(user_id)
 
     # Отправляем сообщение с временной клавиатурой и получаем его объект
-    sent_message = await message.answer('Список избранных квартир')
+    sent_message = await message.answer(LEXICON['favorite_list'])
 
     # Теперь у нас есть message_id
     message_id = sent_message.message_id
@@ -86,14 +87,28 @@ async def favorites_command(message: Message):
     keyboard = favorites_kb(liked_apartments, user_id, message_id=message_id)
 
     # Редактируем сообщение, добавляя клавиатуру
-    await sent_message.edit_text('Список избранных квартир', reply_markup=keyboard)
+    await sent_message.edit_text(LEXICON['favorite_list'], reply_markup=keyboard)
 
 
-@router.callback_query(F.data == 'cancel')
-async def process_cancel_press(callback: CallbackQuery):
-    await callback.message.delete()
+@router.message(Command('publish_apartment'))
+async def publish_apartment_command(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(text=LEXICON['choose_city_landlord'])
+    await state.set_state(FSMFillForm.city)
 
 
+@router.message(Command('view_published_apartments'))
+async def view_published_apartments_command(message: Message, state: FSMContext):
+    await state.clear()
+    user_id = message.from_user.id
+    apartments = get_apartments_by_landlord(user_id)
 
+    # Отправляем сообщение с временной клавиатурой и получаем его объект
+    sent_message = await message.answer(LEXICON['published_list'])
+
+    message_id = sent_message.message_id
+
+    keyboard = landlord_apartments_kb(apartments, message_id)
+    await sent_message.edit_text(LEXICON['published_list'], reply_markup=keyboard)
 
 
